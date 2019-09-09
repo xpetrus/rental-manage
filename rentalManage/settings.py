505a638601da6a8eb9fe55f5a5aafee3361b5900
xpetrus <phsiao24@gmail.com>
2019-09-09 15:58:53 -0400

    some bootstrap

--- rentalManage/settings.py
+++ rentalManage/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rentals',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
